1ef6fb800e81dbd83c2f541acaa58589b320bc49
Chris Donnelly <chrisdonnelly@foxs-imac.foxnewsnyc.com>
2014-01-02 17:56:35 -0500

    user can add stocks

--- js/main.js
+++ js/main.js
@@ -1,48 +1,59 @@
+function update($http,$scope){
+
+		$http({
+			method: 'JSONP',
+			url: 'http://www.foxbusiness.com/ajax/quote/'+$scope.stocks.join(",")+'?callback=jcb'
+		});
+
+		function getHeatNum(val){
+			var pos = { xr : 25, xg : 136, xb : 7, yr : 35, yg : 248, yb : 0,n : 100},
+			neg = { xr : 255, xg : 78, xb : 0, yr : 202, yg : 1,  yb : 1, n : 100
+			}, cObj = {};
+
+			cObj = (parseInt(val) > 0)? pos : neg;
+			var pos = parseInt((Math.round((Math.abs(val)/12)*100)).toFixed(0));
+			red = parseInt((cObj.xr + (( pos * (cObj.yr - cObj.xr)) / (cObj.n-1))).toFixed(0));
+			green = parseInt((cObj.xg + (( pos * (cObj.yg - cObj.xg)) / (cObj.n-1))).toFixed(0));
+			blue = parseInt((cObj.xb + (( pos * (cObj.yb - cObj.xb)) / (cObj.n-1))).toFixed(0));
+			clr = 'rgb('+red+','+green+','+blue+')';
+			return clr;
+		}
+		
+		$scope.stockObjs = [];
+
+		window.jcb = function(d){
+			for(var i = 0; i < d.quote.length; i++){
+				d.quote[i].changeClass = (parseInt(d.quote[i].percentChange) > 0)? "up" : "down";
+				d.quote[i].colorC = getHeatNum(parseInt(d.quote[i].percentChange));
+				$scope.stockObjs.push(d.quote[i]);
+				var elm = document.getElementById("color");
+			}
+		}
+
+
+}
+
+
 var app = angular.module('myApp', []);
 
 app.controller('stockWrap', ['$scope', '$http', function($scope, $http) {
-
-			$scope.getCSSbackgrd =  "background-color:rgb(242,59,0)";
 			
 	$scope.stocks= ["FB","TWTR"];
 
-	$http({
-		method: 'JSONP',
-		url: 'http://www.foxbusiness.com/ajax/quote/'+$scope.stocks.join(",")+'?callback=jcb'
-	});
-
-	function getHeatNum(val){
-		val = Math.abs(val);
-		var pos = parseInt((Math.round((val/12)*100)).toFixed(0));
-		red = parseInt((xr + (( pos * (yr - xr)) / (n-1))).toFixed(0));
-		green = parseInt((xg + (( pos * (yg - xg)) / (n-1))).toFixed(0));
-		blue = parseInt((xb + (( pos * (yb - xb)) / (n-1))).toFixed(0));
-		clr = 'rgb('+red+','+green+','+blue+')';
-		return clr;
+	$scope.update = function(){ 
+		update($http,$scope);
 	}
-	var xr = 255, xg = 78, xb = 0, yr = 202, yg = 1,  yb = 1,n = 100;
-	$scope.stockObjs = [];
 
-	window.jcb = function(d){
-		for(var i = 0; i < d.quote.length; i++){
-			d.quote[i].changeClass = (parseInt(d.quote[i].percentChange) > 0)? "up" : "down";
-			console.log(getHeatNum(parseInt(d.quote[i].percentChange),"xxxx"))
-			//d.quote[i].colorC = getHeatNum(parseInt(d.quote[i].percentChange));
-			$scope.stockObjs.push(d.quote[i]);
-			var elm = document.getElementById("color");
+	$scope.update();
 
-		
-			//$scope.divColor = d.quote[i].colorC;
-
-			//elm.style.backgroundColor = d.quote[i].colorC;
-			
-		}
-		console.log($scope.stockObjs,"stockObjs");
-	}
 }]);
 
-app.controller('stockWrap2', ['$scope', function($scope) {}]);
-
-
+app.controller('stockControl', ['$scope', function($scope) {
+	$scope.addStock = {};
+	$scope.sayStock = function() {
+		$scope.stocks.push($scope.addStock.name.toUpperCase());
+    	$scope.update();
+  	}
+}]);
 
 
