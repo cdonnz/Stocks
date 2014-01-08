88e3d6bd893da1c297f93549e0c2a62400a1af50
Chris Donnelly <chrisdonnelly@Foxs-iMac.local>
2014-01-08 17:36:59 -0500

    cookies started

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,41 @@
+(function(){
+    var utils = {};
+    
+    utils.cookies = (function(){
+        return {
+            add: function(name,val){
+                var expires = "";
+                console.log(name,val)
+                document.cookie = name+"="+val+expires+"; path=/";
+            },
+            delete: function(){
+
+            }
+
+        }
+
+    }());
+
+    utils.heatMap = (function(){
+        return {
+            getRGB: function(val){
+                var val = parseInt(val), n = 100, r = 233, g = 250, b = 232, pos = {r2 : 71, g2 : 226, b2 : 64},
+                neg = {r2 : 207, g2 : 11, b2 : 28}, cObj = {}, p = parseInt, pos;
+
+                cObj = (p(val) > 0)? pos : neg; 
+
+                pos = p((Math.round((Math.abs(val)/7)*100)).toFixed(0));
+                red = p((r + (( pos * (cObj.r2 - r)) / (n-1))).toFixed(0));
+                green = p((g + (( pos * (cObj.g2 - g)) / (n-1))).toFixed(0));
+                blue = p((b + (( pos * (cObj.b2 - b)) / (n-1))).toFixed(0));
+                
+                return 'rgb('+red+','+green+','+blue+')';
+            }
+        }
+})();
+
+utils.cookies.add("foo","xxx");
+
 var app = angular.module('myApp', []);
 
 app.factory('items', function() {
@@ -46,27 +84,18 @@ app.factory('stocks',['$http',function($http){
 }]);
 
 app.controller('stockWrap', ['$scope','items','stocks','$http', function($scope,items,stocks,$http) {
-        var getHeat = function(val){
-            var n = 100, r = 233, g = 250, b = 232, pos = {r2 : 71, g2 : 226, b2 : 64},
-            neg = {r2 : 207, g2 : 11, b2 : 28}, cObj = {}, p = parseInt;
 
-            cObj = (p(val) > 0)? pos : neg;
-            var pos = p((Math.round((Math.abs(val)/7)*100)).toFixed(0));
-            red = p((r + (( pos * (cObj.r2 - r)) / (n-1))).toFixed(0));
-            green = p((g + (( pos * (cObj.g2 - g)) / (n-1))).toFixed(0));
-            blue = p((b + (( pos * (cObj.b2 - b)) / (n-1))).toFixed(0));
-            return 'rgb('+red+','+green+','+blue+')';
-        } 
     //if stocks in url - items.add()
     //else if stocks in cookie - items.add();
     var fn = function(stObjs){
   
         for(var i = 0; i < stObjs.length; i++){
-            stObjs[i].stockColor = getHeat(parseInt(stObjs[i].percentChange));
+            stObjs[i].stockColor = utils.heatMap.getRGB(stObjs[i].percentChange);
         }
         $scope.stockObjs  = stObjs;
     }
     items.add("nq");
+
     stocks.setUpdater(fn);
     stocks.getAll(items.list());
   
@@ -78,3 +107,5 @@ app.controller('adder', ['$scope','items','stocks','$http', function($scope,item
         stocks.getAll(items.list(),$scope);
     }
 }]);
+
+}());
