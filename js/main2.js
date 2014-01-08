4fc9ae2338266520185f91d7c16f503691d4c435
donnobot <chrisdnnlly@yahoo.com>
2014-01-07 23:22:49 -0500

    heatmap working better

--- js/main2.js
+++ js/main2.js
@@ -46,13 +46,30 @@ app.factory('stocks',['$http',function($http){
 }]);
 
 app.controller('stockWrap', ['$scope','items','stocks','$http', function($scope,items,stocks,$http) {
-    
-    //items.add("fb"); items.add("nq");
+        var getHeat = function(val){
+            var n = 100, r = 233, g = 250, b = 232, pos = {r2 : 71, g2 : 226, b2 : 64},
+            neg = {r2 : 207, g2 : 11, b2 : 28}, cObj = {}, p = parseInt;
+
+            cObj = (p(val) > 0)? pos : neg;
+            var pos = p((Math.round((Math.abs(val)/7)*100)).toFixed(0));
+            red = p((r + (( pos * (cObj.r2 - r)) / (n-1))).toFixed(0));
+            green = p((g + (( pos * (cObj.g2 - g)) / (n-1))).toFixed(0));
+            blue = p((b + (( pos * (cObj.b2 - b)) / (n-1))).toFixed(0));
+            return 'rgb('+red+','+green+','+blue+')';
+        } 
     //if stocks in url - items.add()
     //else if stocks in cookie - items.add();
-    var fn = function(stObjs){$scope.stockObjs  = stObjs;}
+    var fn = function(stObjs){
+  
+        for(var i = 0; i < stObjs.length; i++){
+            stObjs[i].stockColor = getHeat(parseInt(stObjs[i].percentChange));
+        }
+        $scope.stockObjs  = stObjs;
+    }
+    items.add("nq");
     stocks.setUpdater(fn);
     stocks.getAll(items.list());
+  
 }]);
 
 app.controller('adder', ['$scope','items','stocks','$http', function($scope,items,stocks,$http) {   
